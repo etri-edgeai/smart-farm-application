dd755ea55bc48f37a6ef8c8fc7665553cbfdf6e3
liang <fconliang@farmconnect.co.kr>
2023-02-10 16:51:53 +0900

    fix(api): fix sensor header and body

--- apps/api/src/app/controllers/sensor.service.ts
+++ apps/api/src/app/controllers/sensor.service.ts
@@ -2,22 +2,22 @@ import { Injectable, Logger } from '@nestjs/common';
 import { InjectQueue } from '@nestjs/bull';
 import { Queue } from 'bull';
 import { Request } from 'express';
-import { SensorJobDataDto } from '@libs/models/sensor';
-import { StringUtils } from '@libs/utils';
+import { SensorJobDataDto } from '@libs/models';
+import { CommService } from '@libs/comm';
 
 @Injectable()
 export class SensorService {
-  constructor(@InjectQueue('sensor') private sensorQueue: Queue<SensorJobDataDto>) { }
+  constructor(@InjectQueue('sensor') private sensorQueue: Queue<SensorJobDataDto>, private commService: CommService) { }
 
   async addToQueue(req: Request, isNew = true) {
     const originalUrl = req.originalUrl;
-    const headers = req.headers;
-    let body = await StringUtils.getJsonBody(req);
+    let headers = CommService.getSimpleHeaders(req);
+    let body = await this.commService.getJsonBody(req);
 
     await this.sensorQueue.add({originalUrl, headers, body});
     const oldNew = isNew ? "New" : "Old";
     const url = isNew ? "" : originalUrl;
-    Logger.debug(`Added to queue(${oldNew}):${req.ip} ${url} ${JSON.stringify(body)}`);
+    Logger.debug(`Added to queue(${oldNew}): ${url} ${JSON.stringify(CommService.getSimpleHeaders(req))} ${JSON.stringify(body)}`);
 
     return 'success';
   }
