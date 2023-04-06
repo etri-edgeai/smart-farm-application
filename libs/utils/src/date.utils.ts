e096de52be394aa6c5d7dd52b909e9ab1754dadd
liang <fconliang@farmconnect.co.kr>
2023-04-06 16:47:26 +0900

    refactor(utils): refactor utils lib

--- libs/utils/src/date.utils.ts
+++ libs/utils/src/date.utils.ts
@@ -1,5 +1,7 @@
-import * as moment from 'moment';
-import { start } from 'repl';
+// 모든 시간관련 로직은 time zone을 고려해야 한다
+// date, string 변환을 format으로 하는게 낫겠다. 그리고 format에 대한 options list를 만들어 줘서 선택하는 방식도 괜찮을듯
+
+import moment from 'moment';
 import { getTimes } from './sunrise/sunrise';
 
 export enum AMPM_TYPE {
@@ -12,7 +14,12 @@ export class DateUtils {
   private static defaultLatitude = 37.5263625;
   private static defaultLongitude = 126.9404481;
 
+  public static getMoment() {
+    return moment;
+  }
+
   public static toMoment(date: moment.MomentInput) {
+    // moment(date) 로 할 수도 있지만, 퍼포먼스를 위해 isMoment()로 체크한다
     return moment.isMoment(date) ? date : moment(date);
   }
 
@@ -29,12 +36,32 @@ export class DateUtils {
     return DateUtils.toMoment(date).format(format);
   }
 
+  public static toDateString(date: moment.MomentInput) {
+    return DateUtils.format(date, DateUtils.simpleDateFormat);
+  }
+
+  public static toTimeString(date: moment.MomentInput) {
+    return DateUtils.format(date, DateUtils.simpleTimeFormat);
+  }
+
+  public static toDateTimeString(date: moment.MomentInput) {
+    return DateUtils.toDateString(date) + ' ' + DateUtils.toTimeString(date);
+  }
+
   public static from(dateString: string, format: string = DateUtils.simpleDateFormat + ' ' + DateUtils.simpleTimeFormat) {
     return moment(dateString, format);
   }
 
-  public static getSunriseSunset(date: Date, latitude?: number, longitude?: number) {
-    const times = getTimes(date, latitude || this.defaultLatitude, longitude || this.defaultLongitude);
+  /**
+   * 일출/일몰
+   * @param date 일자
+   * @param latitude 위도
+   * @param longitude 경도
+   * @returns [일출, 일몰]
+   */
+  public static getSunriseSunset(date: moment.MomentInput, latitude?: number, longitude?: number) {
+    const _date = DateUtils.toDate(date);
+    const times = getTimes(_date, latitude || this.defaultLatitude, longitude || this.defaultLongitude);
     return [times.sunrise, times.sunset];
   }
 
@@ -78,20 +105,23 @@ export class DateUtils {
     }
   }
 
-  public static diff(startDate: moment.MomentInput, endDate: moment.MomentInput, unitOfTime: moment.unitOfTime.Diff = "days", precise?: boolean) {
-    return DateUtils.toMoment(endDate).diff(DateUtils.toMoment(startDate), unitOfTime, precise)
+  static diff(date1: moment.MomentInput, date2: moment.MomentInput = new Date(), unit: moment.unitOfTime.Diff = 'd'): number {
+    const d1 = DateUtils.toMoment(date1);
+    const d2 = DateUtils.toMoment(date2);
+    return d1.diff(d2, unit);
   }
 
-  public static getDatesByRange(startDate: Date, endDate: Date = new Date()) {
-    const dates = [];
-
-    let date = new Date(startDate);
-
-    for (;date <= endDate;) {
-      dates.push(date);
-      date = DateUtils.add(date, 1, 'd').toDate();
-    }
-
-    return dates;
+  static diffS(date1: moment.MomentInput, date2: moment.MomentInput = new Date()): number {
+    const d1 = DateUtils.toMoment(date1);
+    const d2 = DateUtils.toMoment(date2);
+    return d1.diff(d2, 'seconds');
   }
+
+  static diffM(date1: moment.MomentInput, date2: moment.MomentInput = new Date()): number {
+      const d1 = DateUtils.toMoment(date1);
+      const d2 = DateUtils.toMoment(date2);
+      return d1.diff(d2, 'minutes');
+  }
+
 }
+
--- libs/utils/src/index.ts
+++ libs/utils/src/index.ts
@@ -1,5 +1,3 @@
-export * from '.';
-export * from './file.utils';
 export * from './string.utils';
 export * from './date.utils';
 export * from './calc.utils';
